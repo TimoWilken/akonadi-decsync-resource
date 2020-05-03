086ec30128b1643fa262f2e21db4573dfd45c2bb
Timo Wilken <timo.21.wilken@gmail.com>
2020-05-03 12:55:18 +0100

    Set include path so flycheck is useful.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,18 @@
-((nil . ((compile-command . "
+((nil . ((flycheck-clang-include-path
+          . ("/usr/include/qt/"
+             "/usr/include/qt/QtCore/"
+             "/usr/include/qt/QtDBus/"
+             "/usr/include/qt/QtNetwork/"
+             "/usr/include/qt/QtWidgets/"
+             "/usr/include/qt/QtGui/"
+             "/usr/include/KF5/KCoreAddons/"
+             "/usr/include/KF5/KConfigCore/"
+             "/usr/include/KF5/KConfigGui/"
+             "/usr/include/KF5/KI18n/"
+             "/usr/include/KF5/AkonadiCore/"
+             "/usr/include/KF5/AkonadiAgentBase/"))
+
+         (compile-command . "
 set -e
 # cd to project root, which contains .dir-locals.el
 startdir=$PWD
