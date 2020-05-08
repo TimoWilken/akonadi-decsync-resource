278a637465af7ec1b4fdaf96032bf3c0d34146f7
Timo Wilken <timo.21.wilken@gmail.com>
2020-05-08 19:47:00 +0100

    Wait for akonaditest to write environment file.

--- test/test.sh
+++ test/test.sh
@@ -1,11 +1,28 @@
-#!/bin/sh
+#!/usr/bin/bash
+msg() {
+    printf '\n\n' >&2
+    printf ' ==> %s\n' "$@" >&2
+    printf '\n\n' >&2
+}
+
+msg 'REPLACING TEST DECSYNC DATA'
 rm -rf xdgdata/decsync &&
     cp -a ~/sync/decsync xdgdata/decsync ||
         exit 1
 
+msg 'RUNNING AKONADITEST'
+rm -f testenv.sh
 akonaditest -c config.xml --testenv testenv.sh &
-sleep 1s
+
+msg 'WAITING...'
+until [ -f testenv.sh ]; do sleep 1s; done
+
+msg 'SOURCING TESTENV.SH'
 . ./testenv.sh || exit 2
+
+msg 'RUNNING AKONADICONSOLE'
 akonadiconsole
+
+msg 'SHUTTING DOWN'
 shutdown-testenvironment
 wait
